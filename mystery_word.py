48676fd33e8386df31cbcbe0e3ce777fc036d014
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-15 16:07:47 -0400

    Reads in file, function round() runs until game is done

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,47 @@
+import random
+
+
 def play_game():
-    pass
+    # pull in word
+    with open('words.txt', 'r') as input_file:
+        contents = input_file.read()
+
+    # pick word, create list of letters
+    word_list = contents.split()
+    word = random.choice(word_list)
+    # letters = list(word)
+
+    # create board and list of letters
+    length = len(word)
+    board = "_ "*length
+    # board_letters = list(board)
+
+    # game dictionary
+    game = {
+        'guess_left': 8,
+        'letters': list(word),
+        'board_letters': list(board)
+    }
+
+    # take guess
+    # update board
+    while (game['guess_left'] > 0 and '_' in game['board_letters']):
+        round(game)
+
+
+# tracks action for one round
+def round(game):
+    print(''.join(game['letters']))
+    print(''.join(game['board_letters']))
+    letter_guess = input(
+        'Guess a letter: ')
+    if (letter_guess in game['letters']):
+        for i in range(0, len(game['letters'])):
+            if game['letters'][i] == letter_guess:
+                game['board_letters'][i*2] = letter_guess
+    else:
+        game['guess_left'] -= 1
+    print(f'Guesses remaining: {game["guess_left"]}')
 
 
 if __name__ == "__main__":
